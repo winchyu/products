0c2b82b60f3c08b62fbf659163a51416edb75daa
winchyu <winchyu@gmail.comm>
2020-04-07 01:24:23 +0800

    add check file exist

---
+++ products-4.py
@@ -0,0 +1,54 @@
+ # 檢查檔案在不在
+import os # operating system 作業系統
+
+products = [] # 建立一個空清單
+
+if os.path.isfile('products-4.csv'): # 檢查檔案在不在
+	print('yeah! 找到檔案了！')
+	with open('products-4.csv', 'r', encoding= 'utf-8') as f:
+		for line in f:
+			if '商品, 價格' in line:
+				continue # 繼續
+				# 只能在迴圈，表繼續跳到下一迴
+				# 不往下走，直接回到if 的下一迴
+			name, price = line.strip().split(',')
+			# split 切割完的結果是清單
+			products.append([name, price])
+	print(products)
+
+
+
+
+else:
+	print('找不到檔案....')
+
+
+
+
+# 讓使用者輸入
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':    # quit 離開
+		break
+	price = input('請輸入商品價格: ')
+	price = int(price) 
+	products.append([name, price])
+print(products)
+
+
+# 印出所有購買紀錄
+for p in products: 
+	print(p)    
+
+for p in products:
+	print(p[0]) 
+
+for p in products:
+	print(p[0], '的價格是', p[1])
+
+
+# 寫入檔案
+with open('products-4.csv','w', encoding= 'utf-8') as f:
+	f.write('商品, 價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
